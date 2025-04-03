fcaad582397b7c1c08ce9982adb29aaeb914a359
Pratyush Sinha <pratisinha@gmail.com>
2025-04-04 00:21:47 +0530

    made changes to the token.js and env.js file

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import { JWT_EXPIRES_IN, JWT_TOKEN } from '../config/env.js';
+import { JWT_EXPIRES_IN, JWT_TOKEN, NODE_ENV } from '../config/env.js';
 
 export const generateToken = (userId, res) => {
     const token = jwt.sign({ id: userId }, JWT_TOKEN, { expiresIn: JWT_EXPIRES_IN });
@@ -8,6 +8,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true, // prevent XSS attacks cross-site scripting attacks
         sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+        secure: NODE_ENV === "production" || false
     });
 
     return token;
